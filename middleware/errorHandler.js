2c989392decc4a032f4204094354d1cefba469e4
FranklinThaker <franklin.thaker@bacancytechnology.com>
2019-09-26 19:44:50 +0530

    modified

---
+++ middleware/errorHandler.js
@@ -0,0 +1,13 @@
+const { errorResponse } = require('../helpers/helpers');
+
+exports.errorHandler = (err, req, res, next) => {
+  if (err && err.message === 'validation error') {
+    let messages = err.errors.map(e => e.field);
+    if (messages.length && messages.length > 1) {
+      messages = `${messages.join(', ')} are required fields`;
+    } else {
+      messages = `${messages.join(', ')} is required field`;
+    }
+    return errorResponse(req, res, messages, 400, err);
+  }
+};
