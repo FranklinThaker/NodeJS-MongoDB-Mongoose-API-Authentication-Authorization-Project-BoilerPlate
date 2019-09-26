ba3c5c69d0dce09b2d8defd62f6cbaa0915bb857
FranklinThaker <franklin.thaker@bacancytechnology.com>
2019-09-26 19:46:50 +0530

    validator added in routing, rememberMe token verification added in routing

--- routes/routing.js
+++ routes/routing.js
@@ -1,14 +1,18 @@
 // all middlewares define here
-const { authentication, authorization } = require('../middleware/middleware.js');
+const { authentication, authorization, rememberMeTokenVerification } = require('../middleware/middleware.js');
+var validate = require('express-validation')
+var userValidator = require('../controllers/user/user.validator')
+const { errorHandler } = require('../middleware/errorHandler')
 
 // All controllers define here
 const user = require('../controllers/user/user.controller');
 
 const routes = (app) => {
-  app.post('/api/users/', user.register);
-  app.post('/api/users/login', user.login);
+  app.post('/api/users/', validate(userValidator.regsiter), user.register);
+  app.post('/api/users/login', rememberMeTokenVerification, validate(userValidator.login), user.login);
   app.get('/api/users/', authentication, authorization, user.getAll);
   app.get('/api/users/:id', authentication, authorization, user.findById);
+  app.use(errorHandler)
 };
 
 module.exports = { routes };
